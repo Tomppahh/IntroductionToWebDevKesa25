85591f725bc0d23d6fda8b8dc2239a0b5cd231fe
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2025-06-06 11:35:41 +0300

    Initial commit

---
+++ Week 1/app.js
@@ -0,0 +1,19 @@
+const thisButton = document.getElementById('my-button');
+const header1 = document.getElementById('header1');
+const unorderedlist = document.getElementById('my-list'); 
+const addDataButton = document.getElementById('add-data');
+const textArea = document.getElementById('addtextarea');
+const myList = document.querySelector('my-list');
+thisButton.addEventListener("click", printToConsole);
+
+function printToConsole(){
+    let hellotext = "hello world";
+    console.log(hellotext);
+    header1.innerText = "Moi maailma"
+
+}
+
+function addWrittenText(){
+    let textToAdd = textArea.
+
+}
